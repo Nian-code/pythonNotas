b0ab0a8cbe3aa01d8d2d852a6e9a5aac94cb0f6d
Nicoll Angulo <nianmejia16@gmail.com>
2020-12-15 10:52:00 -0500

    Números primos

--- PotenciaPyC/factorial.py
+++ PotenciaPyC/factorial.py
@@ -1,9 +1,17 @@
-def fact(n, f):
-    if n == 1:
-        return f
-    else:
-        return fact(n -1 , f * (n - 1))
+# def fact(n, f):
+#     if n == 1:
+#         return f
+#     else:
+#         return fact(n -1 , f * (n - 1))
+
+# num = int(input("Ingrese el número para hallar su factorial: "))
+# factorial = fact(num, num)
+# print(factorial)
+
+
+from math import factorial
 
 num = int(input("Ingrese el número para hallar su factorial: "))
-factorial = fact(num, num)
-print(factorial)
+
+fac = factorial(num)
+print(fac)
